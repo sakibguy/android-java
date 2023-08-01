d0b1910ec161d37670ac1c085f2d56230c2193f1
sakibguy <sakib.rahman.0000@gmail.com>
2023-08-01 16:37:29 +0600

    obstacle 40: SOLARGUIDE

--- android-techstack/ViewPager/app/src/main/java/com/example/viewpager/MainActivity.java
+++ android-techstack/ViewPager/app/src/main/java/com/example/viewpager/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.viewpager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ArrayList<Integer> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        arrayList.add(R.drawable.icon_11);
+        arrayList.add(R.drawable.icon_21);
+        arrayList.add(R.drawable.icon_31);
+        arrayList.add(R.drawable.icon_41);
+        arrayList.add(R.drawable.icon_51);
+        arrayList.add(R.drawable.icon_61);
+        arrayList.add(R.drawable.icon_71);
+        arrayList.add(R.drawable.icon_81);
+        arrayList.add(R.drawable.icon_91);
+
+        MyAdapter myAdapter =
+                new MyAdapter(MainActivity.this, arrayList);
+        viewPager.setAdapter(myAdapter);
     }
 }
